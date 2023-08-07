e3ce23767aac2554b8ba51afd16eec30383f2a34
toufeeqgoudia <toufeeqgoudia@gmail.com>
2023-08-07 19:25:44 +0200

    working on todo

--- family-planner-app/src/Components/Todo/Todo.jsx
+++ family-planner-app/src/Components/Todo/Todo.jsx
@@ -1,13 +1,76 @@
-
+import { useState, useEffect } from "react";
+import { supabase } from "../../Config/supabaseClient";
+import { useAuth } from "../../Hooks/useAuth";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import TodoList from "./TodoList";
 
 const Todo = () => {
+  const [name, setName] = useState("");
+  const [todos, setTodos] = useState([]);
+  const { user } = useAuth()
 
-    return (
-        <div className="mt-20 mb-10">
-            <h2>Todo </h2>
+  useEffect(() => {
+    getTodos();
+  }, []);
 
-        </div>
-    )
-}
+  const getTodos = async () => {
+    try {
+      const { data, error } = await supabase.from("todoList").select("*");
 
-export default Todo
+      if (error) throw error;
+      if (data !== null) {
+        setTodos(data)
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const addTodo = async (event) => {
+    event.preventDefault()
+
+    try {
+      const { error } = await supabase.from("todoList").insert({
+        task: name,
+        user_id: user.id,
+      }).single()
+
+      if (error) throw error;
+      window.location.reload()
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  // add image for when there are no todos
+
+  return (
+    <div className="mt-20 mb-10 flex flex-col justify-center items-center">
+      <h2 className="text-xl font-bold text-center">Todo</h2>
+
+      <div className="mt-10 w-full flex justify-center">
+        <form className="flex w-4/5 max-w-2xl" onSubmit={addTodo}>
+          <TextField
+            size="medium"
+            placeholder="Add something to do..."
+            sx={{ width: "80%" }}
+            autoComplete="off"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <Button variant="contained" sx={{ width: "20%" }} type="submit">
+            Add
+          </Button>
+        </form>
+      </div>
+
+      <div className="mt-10 w-full flex flex-col justify-center items-center">
+        {todos.map((todo) => (
+          <TodoList key={todo.id} todo={todo} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
---
+++ family-planner-app/src/Components/Todo/TodoList.jsx
@@ -0,0 +1,82 @@
+import { useState } from "react";
+import { supabase } from "../../Config/supabaseClient";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import CancelIcon from '@mui/icons-material/Cancel';
+import CheckIcon from '@mui/icons-material/Check';
+import PropTypes from "prop-types";
+
+const TodoList = ({ todo }) => {
+  const [editing, setEditing] = useState(false);
+
+  // update todo not working
+
+  const updateTodo = async () => {
+    try {
+      const { error } = await supabase
+        .from("todoList")
+        .update({
+          task: todo.task,
+        })
+        .eq("id", todo.id);
+
+      if (error) throw error;
+      window.location.reload();
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const deleteTodo = async () => {
+    try {
+      const { error } = await supabase
+        .from("todoList")
+        .delete()
+        .eq("id", todo.id);
+
+      if (error) throw error;
+      window.location.reload();
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  return (
+    <>
+      {editing === false ? (
+        <div className="mt-2 flex flex-row justify-between items-center w-4/5 max-w-2xl border-2 border-black rounded-lg">
+          <p className="text-base p-3">{todo.task}</p>
+          <div>
+            <button className="text-base p-3">
+              <DeleteIcon color="error" onClick={() => deleteTodo()} />
+            </button>
+            <button className="text-base p-3" onClick={() => setEditing(true)}>
+              <EditIcon color="action" />
+            </button>
+          </div>
+        </div>
+      ) : (
+        <form onSubmit={() => updateTodo()} className="mt-2 flex flex-row justify-between items-center w-4/5 max-w-2xl border-2 border-black rounded-lg">
+          <input
+            className="w-4/5 text-base p-2 ml-1 outline-none"
+            defaultValue={todo.task}
+          />
+          <div>
+          <button className="text-base p-3" onClick={() => setEditing(false)}>
+              <CancelIcon color="action" />
+            </button>
+            <button className="text-base p-3" type="submit">
+              <CheckIcon color="action" />
+            </button>
+          </div>
+        </form>
+      )}
+    </>
+  );
+};
+
+TodoList.propTypes = {
+  todo: PropTypes.object,
+};
+
+export default TodoList;
