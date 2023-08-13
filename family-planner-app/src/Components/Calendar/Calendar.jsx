a8a26932d5c617a640a6f316dd47d7e6e3e5279e
toufeeqgoudia <toufeeqgoudia@gmail.com>
2023-08-13 12:33:16 +0200

    completed calendar

--- family-planner-app/src/Components/Calendar/Calendar.jsx
+++ family-planner-app/src/Components/Calendar/Calendar.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { supabase } from "../../Config/supabaseClient"
+import { supabase } from "../../Config/supabaseClient";
 import { useAuth } from "../../Hooks/useAuth";
 import {
   add,
@@ -15,30 +15,29 @@ import {
   parseISO,
   startOfToday,
 } from "date-fns";
-import Button from "@mui/material/Button"
-import Dialog from "@mui/material/Dialog"
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
 import TextField from "@mui/material/TextField";
-import Divider from "@mui/material/Divider"
+import Divider from "@mui/material/Divider";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import CalendarMeeting from "./CalendarMeeting";
 
-function classNames(...classes) {
+const classNames = (...classes) => {
   return classes.filter(Boolean).join(" ");
-}
+};
 
 const Calendar = () => {
   let today = startOfToday();
   let [selectedDay, setSelectedDay] = useState(today);
   let [currentMonth, setCurrentMonth] = useState(format(today, "MMM-yyyy"));
   let firstDayCurrentMonth = parse(currentMonth, "MMM-yyyy", new Date());
-  const [meetings, setMeetings] = useState([])
-  const [description, setDescription] = useState('')
-  const [date, setDate] = useState(null)
-  const [startTime, setStartTime] = useState(null)
-  const [endTime, setEndTime] = useState(null)
-  const { user } = useAuth()
-  const [dialogOpen, setDialogOpen] = useState(false)
+  const [meetings, setMeetings] = useState([]);
+  const [description, setDescription] = useState("");
+  const [startDateTime, setStartDateTime] = useState("");
+  const [endDateTime, setEndDateTime] = useState("");
+  const { user } = useAuth();
+  const [dialogOpen, setDialogOpen] = useState(false);
 
   let days = eachDayOfInterval({
     start: firstDayCurrentMonth,
@@ -62,46 +61,48 @@ const Calendar = () => {
           .from("meetings")
           .select("*")
           .eq("user_id", user.id);
-      
-        if (error) throw error
+
+        if (error) throw error;
         if (data) {
-          console.log('data: ', data)
+          setMeetings(data);
         }
       } catch (error) {
-          console.log('Error fetching posts', error.message);
+        console.log("Error fetching posts", error.message);
       }
-    }
+    };
 
-    fetchMeetings()
-  }, [user.id])
+    fetchMeetings();
+  }, [user.id]);
 
   const addMeeting = async () => {
     try {
-        const { error } = await supabase
-            .from("meetings")
-            .insert({
-                user_id: user.id,
-                description: description,
-                startTime: startTime,
-                endTime: endTime,
-                date: date
-            });
-        if (error) throw error
-    } catch (error) {
-        console.log(error.message)
-    }
-  }
+      const { error } = await supabase
+        .from("meetings")
+        .insert({
+          user_id: user.id,
+          description: description,
+          startDateTime: startDateTime,
+          endDateTime: endDateTime,
+        })
+        .single();
 
-  let selectedDayMeetings = meetings.filter((meeting) =>
-    isSameDay(parseISO(meeting.startDatetime), selectedDay)
+      if (error) throw error;
+      window.location.reload();
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const selectedDayMeetings = meetings.filter((meeting) =>
+    isSameDay(parseISO(meeting.startDateTime), selectedDay)
   );
 
   const handleDialog = () => {
-    setDialogOpen(!dialogOpen)
-  }
+    setDialogOpen(!dialogOpen);
+  };
 
   return (
-    <div className="mt-20 mb-10 flex flex-col justify-center items-center">
+    <div className="mt-20 mb-24 flex flex-col justify-center items-center">
       <h2 className="text-xl font-bold text-center">Calendar</h2>
       <Button onClick={handleDialog}>Add Meeting</Button>
 
@@ -109,16 +110,50 @@ const Calendar = () => {
         <div className="p-5 w-80">
           <h4 className="text-base font-semibold pb-2">Add Meeting</h4>
           <Divider />
-            <form className="flex flex-col" onSubmit={addMeeting}>
-                <TextField label="Description" focused autoComplete="off" type="text" margin="normal" required onChange={(e) => setDescription(e.target.value)} />
-                <TextField label="Date" focused type="date" required margin="normal" onChange={(e) => setDate(e.target.value)} />
-                <TextField label="Start Time" focused type="time" required margin="normal" onChange={(e) => setStartTime(e.target.value)} />
-                <TextField label="End Time" focused type="time" required margin="normal" onChange={(e) => setEndTime(e.target.value)} />
-                <div className="pt-2 flex flex-row justify-between items-center">
-                <Button variant="contained" type="button" onClick={handleDialog} sx={{ width: '45%' }}>Cancel</Button>
-                <Button variant="contained" type="submit" sx={{ width: '45%' }}>Save</Button>
-                </div>
-            </form>
+          <form className="flex flex-col" onSubmit={() => addMeeting()}>
+            <TextField
+              label="Description"
+              focused
+              autoComplete="off"
+              type="text"
+              margin="normal"
+              required
+              onChange={(e) => setDescription(e.target.value)}
+            />
+            <TextField
+              label="Start Time"
+              focused
+              type="datetime-local"
+              required
+              margin="normal"
+              onChange={(e) =>
+                setStartDateTime(new Date(e.target.value).toISOString())
+              }
+            />
+            <TextField
+              label="End Time"
+              focused
+              type="datetime-local"
+              required
+              margin="normal"
+              onChange={(e) =>
+                setEndDateTime(new Date(e.target.value).toISOString())
+              }
+            />
+            <div className="pt-2 flex flex-row justify-between items-center">
+              <Button
+                variant="contained"
+                type="button"
+                onClick={handleDialog}
+                sx={{ width: "45%" }}
+              >
+                Cancel
+              </Button>
+              <Button variant="contained" type="submit" sx={{ width: "45%" }}>
+                Save
+              </Button>
+            </div>
+          </form>
         </div>
       </Dialog>
 
@@ -136,7 +171,10 @@ const Calendar = () => {
                   className="-my-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
                 >
                   <span className="sr-only">Previous month</span>
-                  <ChevronLeftIcon className="w-5 h-5" aria-hidden="true" />
+                  <ChevronLeftIcon
+                    sx={{ width: "24px", height: "24px" }}
+                    aria-hidden="true"
+                  />
                 </button>
                 <button
                   onClick={nextMonth}
@@ -144,7 +182,10 @@ const Calendar = () => {
                   className="-my-1.5 -mr-1.5 ml-2 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
                 >
                   <span className="sr-only">Next month</span>
-                  <ChevronRightIcon className="w-5 h-5" aria-hidden="true" />
+                  <ChevronRightIcon
+                    sx={{ width: "24px", height: "24px" }}
+                    aria-hidden="true"
+                  />
                 </button>
               </div>
               <div className="grid grid-cols-7 mt-10 text-xs leading-6 text-center text-gray-500">
@@ -200,7 +241,7 @@ const Calendar = () => {
 
                     <div className="w-1 h-1 mx-auto mt-1">
                       {meetings.some((meeting) =>
-                        isSameDay(parseISO(meeting.startDatetime), day)
+                        isSameDay(parseISO(meeting.startDateTime), day)
                       ) && (
                         <div className="w-1 h-1 rounded-full bg-sky-500"></div>
                       )}
@@ -219,7 +260,7 @@ const Calendar = () => {
               <ol className="mt-4 space-y-1 text-sm leading-6 text-gray-500">
                 {selectedDayMeetings.length > 0 ? (
                   selectedDayMeetings.map((meeting) => (
-                    <CalendarMeeting meeting={meeting} description={description} date={date} startTime={startTime} endTime={endTime} key={meeting.id} />
+                    <CalendarMeeting meeting={meeting} key={meeting.id} />
                   ))
                 ) : (
                   <p>No meetings for today.</p>
@@ -244,3 +285,159 @@ let colStartClasses = [
 ];
 
 export default Calendar;
+
+// import dayjs from "dayjs";
+// import { useState } from "react";
+// // import { supabase } from "../../Config/supabaseClient";
+// // import { useAuth } from "../../Hooks/useAuth";
+// import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+// import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+
+// const generateDate = (
+// 	month = dayjs().month(),
+// 	year = dayjs().year()
+// ) => {
+// 	const firstDateOfMonth = dayjs().year(year).month(month).startOf("month");
+// 	const lastDateOfMonth = dayjs().year(year).month(month).endOf("month");
+
+// 	const arrayOfDate = [];
+
+// 	// create prefix date
+// 	for (let i = 0; i < firstDateOfMonth.day(); i++) {
+// 		const date = firstDateOfMonth.day(i);
+
+// 		arrayOfDate.push({
+// 			currentMonth: false,
+// 			date,
+// 		});
+// 	}
+
+// 	// generate current date
+// 	for (let i = firstDateOfMonth.date(); i <= lastDateOfMonth.date(); i++) {
+// 		arrayOfDate.push({
+// 			currentMonth: true,
+// 			date: firstDateOfMonth.date(i),
+// 			today:
+// 				firstDateOfMonth.date(i).toDate().toDateString() ===
+// 				dayjs().toDate().toDateString(),
+// 		});
+// 	}
+
+// 	const remaining = 42 - arrayOfDate.length;
+
+// 	for (
+// 		let i = lastDateOfMonth.date() + 1;
+// 		i <= lastDateOfMonth.date() + remaining;
+// 		i++
+// 	) {
+// 		arrayOfDate.push({
+// 			currentMonth: false,
+// 			date: lastDateOfMonth.date(i),
+// 		});
+// 	}
+// 	return arrayOfDate;
+// };
+
+// const months = [
+// 	"January",
+// 	"February",
+// 	"March",
+// 	"April",
+// 	"May",
+// 	"June",
+// 	"July",
+// 	"August",
+// 	"September",
+// 	"October",
+// 	"November",
+// 	"December",
+// ];
+
+// function cn(...classes) {
+// 	return classes.filter(Boolean).join(" ");
+// }
+
+// export default function Calendar() {
+// 	const days = ["S", "M", "T", "W", "T", "F", "S"];
+// 	const currentDate = dayjs();
+// 	const [today, setToday] = useState(currentDate);
+// 	const [selectDate, setSelectDate] = useState(currentDate);
+
+// 	return (
+// 		<div className="flex gap-10 sm:divide-x justify-center sm:w-1/2 mx-auto  h-screen items-center sm:flex-row flex-col">
+// 			<div className="w-96 h-96 ">
+// 				<div className="flex justify-between items-center">
+// 					<h1 className="select-none font-semibold">
+// 						{months[today.month()]}, {today.year()}
+// 					</h1>
+// 					<div className="flex gap-10 items-center ">
+// 						<ChevronLeftIcon
+// 							className="w-5 h-5 cursor-pointer hover:scale-105 transition-all"
+// 							onClick={() => {
+// 								setToday(today.month(today.month() - 1));
+// 							}}
+// 						/>
+// 						<ChevronRightIcon
+// 							className="w-5 h-5 cursor-pointer hover:scale-105 transition-all"
+// 							onClick={() => {
+// 								setToday(today.month(today.month() + 1));
+// 							}}
+// 						/>
+// 					</div>
+// 				</div>
+// 				<div className="grid grid-cols-7 ">
+// 					{days.map((day, index) => {
+// 						return (
+// 							<h1
+// 								key={index}
+// 								className="text-sm text-center h-14 w-14 grid place-content-center text-gray-500 select-none"
+// 							>
+// 								{day}
+// 							</h1>
+// 						);
+// 					})}
+// 				</div>
+
+// 				<div className=" grid grid-cols-7 ">
+// 					{generateDate(today.month(), today.year()).map(
+// 						({ date, currentMonth, today }, index) => {
+// 							return (
+// 								<div
+// 									key={index}
+// 									className="p-2 text-center h-14 grid place-content-center text-sm border-t"
+// 								>
+// 									<h1
+// 										className={cn(
+// 											currentMonth ? "" : "text-gray-400",
+// 											today
+// 												? "bg-red-600 text-white"
+// 												: "",
+// 											selectDate
+// 												.toDate()
+// 												.toDateString() ===
+// 												date.toDate().toDateString()
+// 												? "bg-black text-white"
+// 												: "",
+// 											"h-10 w-10 rounded-full grid place-content-center hover:bg-black hover:text-white transition-all cursor-pointer select-none"
+// 										)}
+// 										onClick={() => {
+// 											setSelectDate(date);
+// 										}}
+// 									>
+// 										{date.date()}
+// 									</h1>
+// 								</div>
+// 							);
+// 						}
+// 					)}
+// 				</div>
+// 			</div>
+// 			<div className="h-96 w-96 sm:px-5">
+// 				<h1 className=" font-semibold">
+// 					Schedule for {selectDate.toDate().toDateString()}
+// 				</h1>
+// 				<p className="text-gray-400">No meetings for today.</p>
+// 			</div>
+// 		</div>
+// 	);
+// }
--- family-planner-app/src/Components/Calendar/CalendarMeeting.jsx
+++ family-planner-app/src/Components/Calendar/CalendarMeeting.jsx
@@ -1,89 +1,237 @@
-import { Fragment } from "react"
-import { Menu, Transition } from '@headlessui/react'
-import PropTypes from "prop-types"
-import { format, parseISO } from 'date-fns'
-import CircleIcon from '@mui/icons-material/Circle';
+import { Fragment, useState } from "react";
+import { supabase } from "../../Config/supabaseClient";
+import { Menu, Transition } from "@headlessui/react";
+import PropTypes from "prop-types";
+import { format, parseISO } from "date-fns";
+import CircleIcon from "@mui/icons-material/Circle";
+import TextField from "@mui/material/TextField";
 
-function classNames(...classes) {
-    return classes.filter(Boolean).join(' ')
-  }
+const classNames = (...classes) => {
+  return classes.filter(Boolean).join(" ");
+};
 
-function CalendarMeeting({ meeting }) {
-    let startDateTime = parseISO(meeting.startTime)
-    let endDateTime = parseISO(meeting.endTime)
-  
-    return (
-      <li className="flex justify-center items-center px-4 py-2 space-x-4 group rounded-xl focus-within:bg-gray-100 hover:bg-gray-100">
-        <div className="flex flex-col justify-center items-center">
-          <p className="text-gray-900">{meeting.description}</p>
-          <p className="mt-0.5">
-            <time dateTime={meeting.startTime}>
-              {format(startDateTime, 'h:mm a')}
-            </time>{' '}
-            -{' '}
-            <time dateTime={meeting.endTime}>
-              {format(endDateTime, 'h:mm a')}
-            </time>
-          </p>
-        </div>
-        <Menu
-          as="div"
-          className="relative opacity-0 focus-within:opacity-100 group-hover:opacity-100"
-        >
-          <div>
-            <button className="-m-2 flex items-center rounded-full p-1.5 text-gray-500 hover:text-gray-600">
-              <span className="sr-only">Open options</span>
-              <CircleIcon className="w-6 h-6" aria-hidden="true" />
-            </button>
+const CalendarMeeting = ({ meeting }) => {
+  let startDateTime = parseISO(meeting.startDateTime);
+  let endDateTime = parseISO(meeting.endDateTime);
+  const [editing, setEditing] = useState(false);
+  const [editDescription, setEditDescription] = useState("");
+  const [editStartDateTime, setEditStartDateTime] = useState("");
+  const [editEndDateTime, setEditEndDateTime] = useState("");
+
+  const updateMeeting = async () => {
+    try {
+      const { error } = await supabase
+        .from("meetings")
+        .update({
+          description: editDescription,
+          startDateTime: editStartDateTime,
+          endDateTime: editEndDateTime,
+        })
+        .eq("id", meeting.id);
+
+      if (error) throw error;
+      window.location.reload();
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const deleteMeeting = async () => {
+    try {
+      const { error } = await supabase
+        .from("meetings")
+        .delete()
+        .eq("id", meeting.id);
+
+      if (error) throw error;
+      window.location.reload();
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  return (
+    <>
+      {editing === false ? (
+        <li className="flex items-center px-4 py-2 space-x-4 group rounded-xl focus-within:bg-gray-100 hover:bg-gray-100">
+          <div className="flex-auto">
+            <p className="text-gray-900">{meeting.description}</p>
+            <p className="mt-0.5">
+              <time dateTime={meeting.startDateTime}>
+                {format(startDateTime, "h:mm a")}
+              </time>{" "}
+              -{" "}
+              <time dateTime={meeting.endDateTime}>
+                {format(endDateTime, "h:mm a")}
+              </time>
+            </p>
           </div>
-  
-          <Transition
-            as={Fragment}
-            enter="transition ease-out duration-100"
-            enterFrom="transform opacity-0 scale-95"
-            enterTo="transform opacity-100 scale-100"
-            leave="transition ease-in duration-75"
-            leaveFrom="transform opacity-100 scale-100"
-            leaveTo="transform opacity-0 scale-95"
+          <Menu
+            as="div"
+            className="relative opacity-0 focus-within:opacity-100 group-hover:opacity-100"
           >
-            <Menu.Items className="absolute right-0 z-10 mt-2 origin-top-right bg-white rounded-md shadow-lg w-36 ring-1 ring-black ring-opacity-5 focus:outline-none">
-              <div className="py-1">
-                <Menu.Item>
-                  {({ active }) => (
-                    <a
-                      href="#"
-                      className={classNames(
-                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                        'block px-4 py-2 text-sm'
-                      )}
-                    >
-                      Edit
-                    </a>
-                  )}
-                </Menu.Item>
-                <Menu.Item>
-                  {({ active }) => (
-                    <a
-                      href="#"
-                      className={classNames(
-                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                        'block px-4 py-2 text-sm'
-                      )}
-                    >
-                      Cancel
-                    </a>
-                  )}
-                </Menu.Item>
-              </div>
-            </Menu.Items>
-          </Transition>
-        </Menu>
-      </li>
-    )
-  }
+            <div>
+              <Menu.Button className="-m-2 flex items-center rounded-full p-1.5 text-gray-500 hover:text-gray-600">
+                <span className="sr-only">Open options</span>
+                <CircleIcon
+                  sx={{ width: "24px", height: "24px" }}
+                  aria-hidden="true"
+                />
+              </Menu.Button>
+            </div>
 
-  CalendarMeeting.propTypes = {
-    meeting: PropTypes.object
-  }
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            >
+              <Menu.Items className="absolute right-0 z-10 mt-2 origin-top-right bg-white rounded-md shadow-lg w-36 ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <div className="py-1">
+                  <Menu.Item>
+                    {({ active }) => (
+                      <p
+                        onClick={() => setEditing(true)}
+                        className={classNames(
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "block px-4 py-2 text-sm"
+                        )}
+                      >
+                        Edit
+                      </p>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <p
+                        onClick={() => deleteMeeting()}
+                        className={classNames(
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "block px-4 py-2 text-sm"
+                        )}
+                      >
+                        Delete
+                      </p>
+                    )}
+                  </Menu.Item>
+                </div>
+              </Menu.Items>
+            </Transition>
+          </Menu>
+        </li>
+      ) : (
+        <form
+          onSubmit={() => updateMeeting()}
+          className="flex items-center px-4 py-2 space-x-4 group rounded-xl focus-within:bg-gray-100 hover:bg-gray-100"
+        >
+          <div className="flex-auto">
+            <TextField
+              label="Description"
+              focused
+              size="small"
+              autoComplete="off"
+              type="text"
+              margin="normal"
+              required
+              onChange={(e) => setEditDescription(e.target.value)}
+            />
+            <TextField
+              label="Start Time"
+              focused
+              size="small"
+              type="datetime-local"
+              required
+              margin="normal"
+              onChange={(e) =>
+                setEditStartDateTime(new Date(e.target.value).toISOString())
+              }
+            />
+            <TextField
+              label="End Time"
+              focused
+              size="small"
+              type="datetime-local"
+              required
+              margin="normal"
+              onChange={(e) =>
+                setEditEndDateTime(new Date(e.target.value).toISOString())
+              }
+            />
+          </div>
+          <Menu
+            as="div"
+            className="relative opacity-0 focus-within:opacity-100 group-hover:opacity-100"
+          >
+            <div>
+              <Menu.Button className="-m-2 flex items-center rounded-full p-1.5 text-gray-500 hover:text-gray-600">
+                <span className="sr-only">Open options</span>
+                <CircleIcon
+                  sx={{ width: "24px", height: "24px" }}
+                  aria-hidden="true"
+                />
+              </Menu.Button>
+            </div>
 
-  export default CalendarMeeting
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            >
+              <Menu.Items className="absolute right-0 z-10 mt-2 origin-top-right bg-white rounded-md shadow-lg w-36 ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <div className="py-1">
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        type="submit"
+                        className={classNames(
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "block px-4 py-2 text-sm"
+                        )}
+                      >
+                        Save
+                      </button>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <p
+                        onClick={() => setEditing(false)}
+                        className={classNames(
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "block px-4 py-2 text-sm"
+                        )}
+                      >
+                        Cancel
+                      </p>
+                    )}
+                  </Menu.Item>
+                </div>
+              </Menu.Items>
+            </Transition>
+          </Menu>
+        </form>
+      )}
+    </>
+  );
+};
+
+CalendarMeeting.propTypes = {
+  meeting: PropTypes.object,
+};
+
+export default CalendarMeeting;
